b0765c64f4a426f3761255135c7973fba24b82eb
Shawn Silverman <shawn@pobox.com>
2026-03-15 09:29:26 -0700

    main: Initialize DCP in setup()
    Also set up Serial.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 
+#include "decept/dcp/dcp.h"
+
 #if !defined(PIO_UNIT_TESTING)
 
 // Main program setup.
 void setup() {
+  Serial.begin(115200);
+  while (!Serial && (millis() < 4000)) {
+    // Wait for Serial
+  }
+
+  // Initialize the DCP module
+  decept::dcp::init();
 }
 
 // Main program loop.

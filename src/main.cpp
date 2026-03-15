14da47999b65686189cf3ef031b5bf17bb586e62
Shawn Silverman <shawn@pobox.com>
2026-03-15 09:30:54 -0700

    main: Add a startup delay and print some messages

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,20 @@
 
 #if !defined(PIO_UNIT_TESTING)
 
+static uint32_t kStartupDelay = 1500;
+
 // Main program setup.
 void setup() {
   Serial.begin(115200);
   while (!Serial && (millis() < 4000)) {
     // Wait for Serial
   }
+  delay(kStartupDelay);
+  printf("Starting...\r\n");
 
   // Initialize the DCP module
   decept::dcp::init();
+  printf("DCP initialized");
 }
 
 // Main program loop.

71b8fc0a277314749317b2c8edc04e122c784b78
Shawn Silverman <shawn@pobox.com>
2026-04-23 20:17:47 -0700

    tests: Add entropy tests
    Also update platformio.ini to include Boost math for the tests and
    enable support for double-typed tests in Unity.

---
+++ test/test_entropy/test_main.cpp
@@ -0,0 +1,265 @@
+// SPDX-FileCopyrightText: (c) 2023-2026 Shawn Silverman <shawn@pobox.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+// test_main.cpp tests the entropy functions.
+// This file is part of the QNEthernet library.
+
+// C++ includes
+#include <array>
+#include <cerrno>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <cstdio>
+#include <limits>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <Arduino.h>
+#undef F  /* Undefine some Arduino nonsense */
+#include <boost/math/distributions/chi_squared.hpp>
+#include <decept/entropy/entropy.h>
+#include <decept/entropy/random_device.h>
+#include <unity.h>
+
+#include "util/OnlineSerialCorrelation.h"
+
+using namespace decept::entropy;
+
+// --------------------------------------------------------------------------
+//  Utilities
+// --------------------------------------------------------------------------
+
+// Formats into a char vector and returns the vector. The vector will always be
+// terminated with a NUL. This returns an empty string if something goes wrong
+// with the print function.
+template <typename... Args>
+static std::vector<char> format(const char* format, Args... args) {
+  std::vector<char> out;
+
+  int size = std::snprintf(nullptr, 0, format, args...) + 1;  // Include the NUL
+  if (size <= 1) {
+    out.resize(1, 0);
+  } else {
+    out.resize(size);
+    TEST_ASSERT_EQUAL_MESSAGE(size - 1,
+                              std::snprintf(out.data(), size, format, args...),
+                              "Expected complete string fill");
+  }
+  return out;
+}
+
+// --------------------------------------------------------------------------
+//  Main Program
+// --------------------------------------------------------------------------
+
+// Pre-test setup. This is run before every test.
+void setUp() {
+}
+
+// Post-test teardown. This is run after every test.
+void tearDown() {
+}
+
+// Tests that the feature is active.
+static void test_active() {
+  TEST_ASSERT_TRUE_MESSAGE(trng_is_started(), "Expected started");
+}
+
+// Tests that the feature is inactive.
+static void test_inactive() {
+  TEST_ASSERT_FALSE_MESSAGE(trng_is_started(), "Expected not started");
+}
+
+// Tests data-available.
+static void test_available() {
+  // Assume we're going to read full entropy
+  delay(100);  // Give it time to collect entropy
+  TEST_ASSERT_EQUAL_MESSAGE(64, trng_available(), "Expected full entropy");
+}
+
+// Tests data access.
+static void test_data() {
+  uint8_t b[64];
+  TEST_ASSERT_EQUAL_MESSAGE(1, trng_data(b, 1), "Expected 1-byte read");
+  TEST_ASSERT_EQUAL_MESSAGE(63, trng_available(), "Expected full entropy");
+  TEST_ASSERT_EQUAL_MESSAGE(63, trng_data(&b[1], 63), "Expected 63-byte read");
+  TEST_ASSERT_EQUAL_MESSAGE(0, trng_available(), "Expected empty entropy");
+}
+
+// Tests entropy_random().
+static void test_random() {
+  errno = 0;
+  (void)entropy_random();
+  TEST_ASSERT_EQUAL_MESSAGE(0, errno, "Expected no error");
+}
+
+// Tests entropy_random_range(range).
+static void test_random_range() {
+  errno = 0;
+  (void)entropy_random_range(0);
+  TEST_ASSERT_EQUAL_MESSAGE(EDOM, errno, "Expected EDOM");
+  errno = 0;
+  TEST_ASSERT_EQUAL_MESSAGE(0, entropy_random_range(1), "Expected zero");
+  TEST_ASSERT_EQUAL_MESSAGE(0, errno, "Expected no error");
+  for (int i = 0; i < (1 << 10); ++i) {
+    uint32_t r = entropy_random_range(10);
+    TEST_ASSERT_EQUAL_MESSAGE(0, errno, "Expected no error");
+    const auto msg = "Expected value < 10: iteration " + std::to_string(i);
+    TEST_ASSERT_LESS_THAN_UINT32_MESSAGE(10, r, msg.c_str());
+  }
+}
+
+// Tests entropy_random().
+static void test_random_device() {
+  decept::entropy::random_device rd;
+
+  TEST_ASSERT_EQUAL_MESSAGE(0, decept::entropy::random_device::min(),
+                            "Expected full-range minimum");
+  TEST_ASSERT_EQUAL_MESSAGE(std::numeric_limits<unsigned int>::max(),
+                            decept::entropy::random_device::max(),
+                            "Expected full-range maximum");
+  TEST_ASSERT_EQUAL_MESSAGE(sizeof(unsigned int) * CHAR_BIT, rd.entropy(),
+                            "Expected full entropy");
+
+  errno = 0;
+  for (int i = 0; i < (1 << 10); ++i) {
+    (void)rd();
+    TEST_ASSERT_EQUAL_MESSAGE(0, errno, "Expected no error");
+  }
+}
+
+// Creates an array of online serial correlation objects having
+// consecutive lags.
+template <size_t... I>
+static constexpr auto make_corrs(std::index_sequence<I...>) {
+  return std::array{util::OnlineSerialCorrelation<uint8_t>{I + 1}...};
+}
+
+// Tests the quality of the entropy by performing some basic tests.
+// These include:
+// 1. Entropy > 7.9
+// 2. Mean between 127 and 128
+// 3. Chi-squared p-value > 0.10
+// 4. Various lagged autocorrelations are all < 0.2
+//
+// See:
+// * https://www.fourmilab.ch/random/
+// * https://github.com/Fourmilab/ent_random_sequence_tester
+static void test_randomness() {
+  static constexpr size_t kBufSize = 64;
+  static constexpr size_t kFillCount = 2*1024;  // 64-byte fills
+  static constexpr size_t kLagCount = 5;
+
+  // Progress bar in increments of 10%.
+  static constexpr char kBar[]{"=========="};
+
+  decept::entropy::random_device rd;
+  uint8_t buf[kBufSize];
+  uint32_t counts[256]{0};
+  uint32_t totalCount = 0;
+  double p[256];  // Probabilities
+
+  // Use different lags
+  auto corrs = make_corrs(std::make_index_sequence<kLagCount>());
+
+  TEST_MESSAGE(
+      format("Starting randomness test: %zu bytes", kFillCount * kBufSize)
+          .data());
+
+  TEST_MESSAGE("8[0%]");
+  for (size_t i = 0; i < kFillCount; ++i) {
+    for (size_t j = 1; j <= 9; ++j) {
+      if (i == j * kFillCount / 10) {
+        TEST_MESSAGE(format("8%s[%zu%%]", kBar + 10 - j, j * 10).data());
+      }
+    }
+
+    TEST_ASSERT_EQUAL(kBufSize, rd(buf, kBufSize));
+    for (size_t j = 0; j < kBufSize; ++j) {
+      ++counts[buf[j]];
+      ++totalCount;
+
+      for (auto& corr : corrs) {
+        corr.sample(buf[j]);
+      }
+    }
+  }
+  TEST_MESSAGE(format("8%sD-", kBar).data());
+
+  const double expectedCount = totalCount / 256.0;
+  double chiSq   = 0.0;
+  double sum     = 0.0;
+  double entropy = 0.0;
+
+  for (size_t i = 0; i < 256; i++) {
+    const double a = counts[i] - expectedCount;
+
+    p[i] = static_cast<double>(counts[i]) / static_cast<double>(totalCount);
+    if (p[i] != 0.0) {
+      entropy += p[i] * -std::log2(p[i]);
+    }
+
+    chiSq += (a * a) / expectedCount;
+    sum += static_cast<double>(i) * counts[i];
+  }
+
+  const double mean = sum / totalCount;
+
+  static const boost::math::chi_squared_distribution<double> dist(255);
+  double pVal = 1.0 - boost::math::cdf(dist, chiSq);
+
+  TEST_MESSAGE(format("entropy = %f", entropy).data());
+  TEST_MESSAGE(format("mean    = %f", mean).data());
+  TEST_MESSAGE(format("chi-sq  = %f", chiSq).data());
+  TEST_MESSAGE(format("p-val   = %f", pVal).data());
+  for (const auto& corr : corrs) {
+    TEST_MESSAGE(
+        format("corr_%zu  = %+f", corr.lag(), corr.correlation()).data());
+  }
+
+  // Tests
+  TEST_ASSERT_GREATER_THAN_DOUBLE(7.9, entropy);
+  TEST_ASSERT_DOUBLE_WITHIN(0.75, 127.5, mean);
+  TEST_ASSERT_GREATER_THAN_DOUBLE(0.10, pVal);
+  for (const auto& corr : corrs) {
+    TEST_ASSERT_LESS_THAN_DOUBLE(0.2, std::abs(corr.correlation()));
+  }
+}
+
+// Main program setup.
+void setup() {
+  Serial.begin(115200);
+  while (!Serial && (millis() < 4000)) {
+    // Wait for Serial
+  }
+
+  // NOTE!!! Wait for >2 secs
+  // if board doesn't support software reset via Serial.DTR/RTS
+  delay(2000);
+
+#if defined(TEENSYDUINO)
+  if (CrashReport) {
+    (void)Serial.println(CrashReport);
+  }
+#endif  // defined(TEENSYDUINO)
+
+  UNITY_BEGIN();
+  RUN_TEST(test_inactive);
+  trng_init();
+  RUN_TEST(test_active);
+  RUN_TEST(test_available);
+  RUN_TEST(test_data);
+  RUN_TEST(test_random);
+  RUN_TEST(test_random_range);
+  RUN_TEST(test_random_device);
+  RUN_TEST(test_randomness);
+  trng_deinit();
+  RUN_TEST(test_inactive);
+  UNITY_END();
+}
+
+// Main program loop.
+void loop() {
+}
